888939cbdaf5a7f294607c55c2b20d47b6c71114
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-22 21:13:26 +1000

    add types

--- item.py
+++ item.py
@@ -1,28 +1,28 @@
 from socket_utils import SocketUtils
 
 class Item:
-    def __init__(self, item_type, request, host, port):
+    def __init__(self, item_type: str, request: str, host: str, port: str):
         self.item_type = item_type
         self.request = request
         self.host = host
         self.port = port
 
-    def get_item_type(self):
+    def get_item_type(self) -> str:
         return self.item_type
     
-    def get_request(self):
+    def get_request(self) -> str:
         return self.request
     
-    def get_host(self):
+    def get_host(self) -> str:
         return self.host
     
-    def get_port(self):
+    def get_port(self) -> str:
         return self.port
     
-    def is_external_server(self, server_host, server_port):
+    def is_external_server(self, server_host: str, server_port: int) -> bool:
         return self.host != server_host or int(self.port) != server_port
     
-    def is_external_server_up(self):
+    def is_external_server_up(self) -> bool:
         try:
             new_sock = SocketUtils.create_socket(self.host, int(self.port))
             new_sock.close()
--- socket_utils.py
+++ socket_utils.py
@@ -1,9 +1,8 @@
 from socket import *
-from datetime import datetime
 
 class SocketUtils:
     @staticmethod
-    def create_socket(host, port):
+    def create_socket(host: str, port: int) -> socket:
         sock = socket(AF_INET, SOCK_STREAM)
         sock.settimeout(5)
         sock.connect((host, port))
