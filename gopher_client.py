fc847dc654100317acf4cb1dea8a2ba6a2acaf10
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-23 20:03:59 +1000

    update statistic

--- gopher_client.py
+++ gopher_client.py
@@ -8,6 +8,9 @@ from time import time
 
 class GopherClient:
     TIME_OUT = 35
+    EOF_TERMINATOR = b".\r\n"
+    CRLF = "\r\n"
+    TAB = "\t"
 
     # constructor
     def __init__(self, server_host, server_port):
@@ -29,12 +32,12 @@ class GopherClient:
 
     def send_request(self, path: str) -> None:
         print(f"Request: {path} @ Time: {datetime.now()}")
-        path += "\r\n"
+        path += GopherClient.CRLF
         self.sock = SocketUtils.create_socket(self.server_host, self.server_port)
         self.sock.sendall(path.encode())
     
 
-    def read_response(self, path: str) -> bytes:
+    def read_response(self, path: str, is_text_file: bool = False) -> bytes:
         data = b""
 
         start_time = time()
@@ -56,7 +59,7 @@ class GopherClient:
                 data += chunk
 
                 # if the response ends with .CRLF, then it is the end of the file
-                if data.endswith(b".\r\n"):
+                if data.endswith(GopherClient.EOF_TERMINATOR):
                     break
 
             except (socket.timeout, TimeoutError):
@@ -68,16 +71,22 @@ class GopherClient:
         # close socket since we only want one-time request
         self.sock.close()
 
+        if is_text_file:
+            if data.endswith(GopherClient.EOF_TERMINATOR):
+                payload = data[:-len(GopherClient.EOF_TERMINATOR)].rstrip(GopherClient.CRLF.encode())
+                return payload
+            else:
+                self.references_with_issues.add(path)
+                return data
+        else:
+            return data
 
-        if data.endswith(b".\r\n"):
-            return data[:-5]
-        return data
     
     def parse_response(self, data: str) -> list[Item]:
         parsed_items = []
 
         # split into an array of lines because each line ends with a CRLF
-        lines = data.split("\r\n")
+        lines = data.split(GopherClient.CRLF)
         for line in lines:
             parsed_item: Item = GopherClient.parse_item(line)
             parsed_items.append(parsed_item)
@@ -88,7 +97,7 @@ class GopherClient:
     @staticmethod
     def parse_item(line: str) -> Item:
         # split into an array of items because each item is separated by a tab
-        raw_items = line.split("\t")
+        raw_items = line.split(GopherClient.TAB)
         item_type = raw_items[0][0] if len(raw_items[0]) > 0 else ""
         path = raw_items[1] if len(raw_items) > 1 else ""
         host = raw_items[2] if len(raw_items) > 2 else ""
@@ -123,48 +132,49 @@ class GopherClient:
                 continue
 
             # item-type 3 (error)
-            elif item_type == "3":
-                self.references_with_errors.add(item_path)
-                return
+            if item_type == "3":
+                self.references_with_errors.add(path)
+                continue
 
             # EXTERNAL SERVER CASE
-            elif item.is_external_server(self.server_host, self.server_port):
+            if item.is_external_server(self.server_host, self.server_port):
                 if is_up := item.is_external_server_up():
                     self.external_servers[item_host] = (item_port, is_up)
-                    return
+                    continue
                 else:
                     self.references_with_issues.add(item_path)
-                    return
+                    continue
             
             # INTERNAL SERVER CASE
             # item-type 0 (text file)
-            elif item_type == "0":
+            if item_type == "0":
                 self.handle_text_file(item_path)
-                return
+
+            # non-text file
+            elif item_type != "i" and item_type != "1":
+                self.handle_non_text_file(item_path)
 
             # item-type 1 (directory)
             elif item_type == "1" and item_path != "" and item_path not in self.directories:
                 self.index_server(item_path)
                 self.directories.add(item_path)
-                return
 
-            # non-text file
-            else:
-                self.handle_non_text_file(item_path)
-                return
+
 
     def handle_text_file(self, item_path: str) -> None:
         self.send_request(item_path)
         try:
-            text_file_content = self.read_response(item_path).decode()
+            text_file_content = self.read_response(item_path, is_text_file=True)
         except:
             return
+        
+        if item_path in self.references_with_issues:
+            return
 
-        if item_path not in self.references_with_issues:
-            self.text_file_path_list.append(item_path)
+        self.text_file_path_list.append(item_path)
 
         if len(text_file_content) < self.smallest_text_file[2]: 
-            self.smallest_text_file = (item_path, text_file_content, len(text_file_content))
+            self.smallest_text_file = (item_path, text_file_content.decode(), len(text_file_content))
 
         if len(text_file_content) > self.largest_text_file[1]:
             self.largest_text_file = (item_path, len(text_file_content))
@@ -173,12 +183,14 @@ class GopherClient:
     def handle_non_text_file(self, item_path: str) -> None:
         self.send_request(item_path)
         try:
-            non_text_file_content = self.read_response(item_path).decode()
+            non_text_file_content = self.read_response(item_path, is_text_file=False)
         except:
             return
 
-        if item_path not in self.references_with_issues:
-            self.binary_file_path_list.append(item_path)
+        if item_path in self.references_with_issues:
+            return
+
+        self.binary_file_path_list.append(item_path)
 
         if len(non_text_file_content) < self.smallest_binary_file[1]:
             self.smallest_binary_file = (item_path, len(non_text_file_content))
@@ -188,47 +200,53 @@ class GopherClient:
 
 
     def print_results(self) -> None:
-        print("\n----- Gopher Server Analysis Results -----\n")
+        print("\n----- Gopher Server Analysis Results -----")
         
         # a. The number of Gopher directories on the server
-        print(f"a. Number of Gopher directories: {len(self.directories)}")
+        print(f"\na. Number of Gopher directories: {len(self.directories)}")
+        print("List of Gopher directories:")
+        for path in self.directories:
+            print(f"{path}")
         
         # b. The number and list of all simple text files (full path)
-        print(f"b. Number of text files: {len(self.text_file_path_list)}")
-        print("   List of text files:")
+        print(f"\nb. Number of text files: {len(self.text_file_path_list)}")
+        print("List of text files:")
         for path in self.text_file_path_list:
-            print(f"   - {path}")
+            print(f"{path}")
         
         # c. The number and list of all binary files (full path)
-        print(f"c. Number of binary files: {len(self.binary_file_path_list)}")
-        print("   List of binary files:")
+        print(f"\nc. Number of binary files: {len(self.binary_file_path_list)}")
+        print("List of binary files:")
         for path in self.binary_file_path_list:
-            print(f"   - {path}")
+            print(f"{path}")
         
         # d. The contents of the smallest text file
-        print(f"d. Contents of the smallest text file ({self.smallest_text_file[0]}):")
-        print(f"   {self.smallest_text_file[1]}")
+        print(f"\nd. Contents of the smallest text file ({self.smallest_text_file[0]}):")
+        print(f"{self.smallest_text_file[1]}")
         
         # e. The size of the largest text file
-        print(f"e. Size of the largest text file ({self.largest_text_file[0]}): {self.largest_text_file[1]} bytes")
+        print(f"\ne. Size of the largest text file ({self.largest_text_file[0]}): {self.largest_text_file[1]} bytes")
         
         # f. The size of the smallest and the largest binary files
-        print(f"f. Size of the smallest binary file ({self.smallest_binary_file[0]}): {self.smallest_binary_file[1]} bytes")
+        print(f"\nf. Size of the smallest binary file ({self.smallest_binary_file[0]}): {self.smallest_binary_file[1]} bytes")
         print(f"   Size of the largest binary file ({self.largest_binary_file[0]}): {self.largest_binary_file[1]} bytes")
         
         # g. The number of unique invalid references (those with an "error" type)
-        print(f"g. Number of unique invalid references: {len(self.references_with_errors)}")
+        print(f"\ng. Number of unique invalid references: {len(self.references_with_errors)}")
+        print("List of invalid references:")
+        for ref in self.references_with_errors:
+            print(f"{ref}")
         
         # h. A list of external servers that were referenced
-        print("h. External servers referenced:")
+        print("\nh. List of external servers referenced:")
         for host, (port, is_up) in self.external_servers.items():
             status = "up" if is_up else "down"
-            print(f"   - {host}:{port} ({status})")
+            print(f"{host}:{port} ({status})")
         
         # i. Any references that have "issues/errors"
-        print("i. References with issues/errors:")
+        print("\ni. References with issues/errors:")
         for ref in self.references_with_issues:
-            print(f"   - {ref}")
+            print(f"{ref}")
         
         print("\n----- End of Analysis -----")
 
